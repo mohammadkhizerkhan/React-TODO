e14f95eeb4f23a21ee76ac575f5c7565845fce7f
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-03-02 21:57:34 +0530

    added addTask component

---
+++ my-app/src/components/AddTask.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AddTask() {
+    return (
+        <div className='AddTask'>
+            <form action="">
+                <input type="text" className='input'/>
+                <button className="addBtn">Add Task</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTask
